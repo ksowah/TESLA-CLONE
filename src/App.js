dcad32eb3ac93b70e804ad5b076ab95c1b2f3661
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-10-12 17:58:04 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
